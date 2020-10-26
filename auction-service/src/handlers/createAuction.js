a6fecff9adc776032f5aef971c227b60dd1c86fa
dineshdsv3 <dineshdsv3@gmail.com>
2020-10-26 16:24:31 +0530

    Schema Validation - CreateAuction

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -2,6 +2,8 @@ import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -40,7 +42,7 @@ async function createAuction(event, context) {
 	};
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction).use(validator({ inputSchema: createAuctionSchema }));
 // .use(httpJsonBodyParser()) // will automatically parse our stringified event body so we no need to give JSON.parse every time
 // .use(httpEventNormalizer()) // will automatically adjust the API gateway event object to prevent us from accidentally having a non existing objects when trying to access path parameters or query parameters when they are not provided. Will save us from room for errors and IF statements
 // .use(httpErrorHandler()); // will make error handling smooth and easy
