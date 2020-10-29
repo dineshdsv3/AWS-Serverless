f26fb8193308eb1d88af73ccf2511af0d350de54
dineshdsv3 <dineshdsv3@gmail.com>
2020-10-29 18:14:11 +0530

    setting picture URL in dynamoDb

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -3,6 +3,7 @@ import httpErrorHandler from '@middy/http-error-handler';
 import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
 
 export async function uploadAuctionPicture(event) {
 	const { id } = event.pathParameters;
@@ -11,9 +12,12 @@ export async function uploadAuctionPicture(event) {
 	const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
 	const buffer = Buffer.from(base64, 'base64');
 
+	let updatedAuction;
+
 	try {
-		const uploadToS3Result = uploadPictureToS3(auction.id + '.jpg', buffer);
-		console.log(uploadToS3Result);
+		const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+		updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+		// Picture URL
 	} catch (error) {
 		console.error(error);
 		throw new createError.InternalServerError(error);
@@ -21,7 +25,7 @@ export async function uploadAuctionPicture(event) {
 
 	return {
 		statusCode: 200,
-		body: JSON.stringify({}),
+		body: JSON.stringify(updatedAuction),
 	};
 }
 
